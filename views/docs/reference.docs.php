2ef44a1eca6dac8626529fb3f74d154f0d0709b1
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-10-20 13:41:52 +0200

    Added return types

--- views/docs/reference.docs.php
+++ views/docs/reference.docs.php
@@ -2,27 +2,27 @@
 <h1>Reference</h1>
 <p>This is an reference of all global variables and functions:</p>
 <pre>
-$debug
+bool      $debug
 
-$router-&gt;redirect($url,$location);
-$router-&gt;getParameters();
-$router-&gt;getTemplate()
-$router-&gt;getAction();
-$router-&gt;getView();
-$router-&gt;getUrl();
-
-$body
-
-$parameters
-
-$db-&gt;q($sql,&hellip;);
-$db-&gt;q1($sql,&hellip;);
-$db-&gt;id();
-$db-&gt;handle();
-
-function redirect($url)
-function parameterless()
-
-function authenticate($username,$password)
-function register($username,$password)
+          $router-&gt;redirect($url,$location);
+string    $router-&gt;getParameters();
+string    $router-&gt;getTemplate()
+string    $router-&gt;getAction();
+string    $router-&gt;getView();
+string    $router-&gt;getUrl();
+        
+string    $body
+        
+array     $parameters
+        
+array     $db-&gt;q($sql,&hellip;);
+array     $db-&gt;q1($sql,&hellip;);
+integer   $db-&gt;id();
+object    $db-&gt;handle();
+        
+          function redirect($url)
+          function parameterless()
+          
+bool      function authenticate($username,$password)
+bool      function register($username,$password)
 </pre>

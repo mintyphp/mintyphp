637f6247d9d00ad6193a46c2a4d438738adbb0b7
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-10-20 18:42:29 +0200

    Improve constraints on registration

--- actions/register.php
+++ actions/register.php
@@ -7,7 +7,13 @@ if (isset($_POST['username']))
 { $username = $_POST['username'];
   $password = $_POST['password'];
   $password2 = $_POST['password2'];
-  if ($password!=$password2) 
+  if (!$username)
+  { $error = "Username cannot be empty";
+  }
+  elseif (!$password)
+  { $error = "Password cannot be empty";
+  }
+  elseif ($password!=$password2) 
   { $error = "Passwords must match"; 
   } 
   elseif (!register($username, $password))

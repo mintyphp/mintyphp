8e4a6b1db2ee6ba7a00587cfd3548a1198f8caf5
maurits <maurits@lx51.nlware.com>
2014-04-18 16:10:30 +0200

    bugfix

--- templates/docs.php
+++ templates/docs.php
@@ -36,11 +36,5 @@
         <div class="body">
             <?php echo Router::getContent(); ?>
         </div>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="/js/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="/js/bootstrap.min.js"></script>
-
 </body>
 </html>

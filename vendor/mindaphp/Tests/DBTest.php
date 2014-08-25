7c10b7fada704ac40150fbc8a7a9fc0c64bc3971
Maurits van der Schee <maurits@vdschee.nl>
2014-08-25 12:17:31 +0200

    Improved layout

--- vendor/mindaphp/Tests/DBTest.php
+++ vendor/mindaphp/Tests/DBTest.php
@@ -27,13 +27,13 @@ class DBTest extends \PHPUnit_Framework_TestCase
 	public function testCreateUsers()
 	{
 		$result = DB::query('CREATE TABLE `users` (
-				`id` int(11) NOT NULL AUTO_INCREMENT,
-				`username` varchar(255) COLLATE utf8_bin NOT NULL,
-				`password` varchar(255) COLLATE utf8_bin NOT NULL,
-				`salt` varchar(255) COLLATE utf8_bin NOT NULL,
-				`created` datetime NOT NULL,
-				PRIMARY KEY (`id`),
-				UNIQUE KEY `username` (`username`)
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`username` varchar(255) COLLATE utf8_bin NOT NULL,
+			`password` varchar(255) COLLATE utf8_bin NOT NULL,
+			`salt` varchar(255) COLLATE utf8_bin NOT NULL,
+			`created` datetime NOT NULL,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `username` (`username`)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;');
 		$this->assertNotFalse($result, 'create users failed: '.DB::$error);
 	}
@@ -41,18 +41,18 @@ class DBTest extends \PHPUnit_Framework_TestCase
 	public function testCreatePosts()
 	{
 		$result = DB::query('CREATE TABLE `posts` (
-		  `id` int(11) NOT NULL AUTO_INCREMENT,
-		  `slug` varchar(255) COLLATE utf8_bin NOT NULL,
-		  `tags` varchar(255) COLLATE utf8_bin NOT NULL,
-		  `title` text COLLATE utf8_bin NOT NULL,
-		  `content` mediumtext COLLATE utf8_bin NOT NULL,
-		  `created` datetime NOT NULL,
-		  `published` datetime DEFAULT NULL,
-		  `user_id` int(11) NOT NULL,
-		  PRIMARY KEY (`id`),
-		  UNIQUE KEY `slug` (`slug`),
-		  KEY `user_id` (`user_id`),
-		  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`slug` varchar(255) COLLATE utf8_bin NOT NULL,
+			`tags` varchar(255) COLLATE utf8_bin NOT NULL,
+			`title` text COLLATE utf8_bin NOT NULL,
+			`content` mediumtext COLLATE utf8_bin NOT NULL,
+			`created` datetime NOT NULL,
+			`published` datetime DEFAULT NULL,
+			`user_id` int(11) NOT NULL,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `slug` (`slug`),
+			KEY `user_id` (`user_id`),
+			CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;');
 		$this->assertNotFalse($result, 'create posts failed: '.DB::$error);
 	}

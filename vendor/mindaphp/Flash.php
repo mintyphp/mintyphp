c61dd031b1652111cdbc4a20bcfaa62799ba5dc7
Maurits van der Schee <maurits@vdschee.nl>
2015-01-12 11:28:55 +0100

    Added flash facility

---
+++ vendor/mindaphp/Flash.php
@@ -0,0 +1,26 @@
+<?php
+namespace MindaPHP;
+
+class Flash
+{
+	static $flashSessionKey = 'flash';
+	
+	public static function set($type,$message)
+	{
+		if (!isset($_SESSION[static::$flashSessionKey])) $_SESSION[static::$flashSessionKey] = array();
+		if (!isset($_SESSION[static::$flashSessionKey][$type])) $_SESSION[static::$flashSessionKey][$type] = array();
+		$_SESSION[static::$flashSessionKey][$type][] = $message;
+	}
+	
+	public static function get()
+	{
+		if (isset($_SESSION[static::$flashSessionKey])) {
+			$flash = $_SESSION[static::$flashSessionKey];
+			unset($_SESSION[static::$flashSessionKey]);
+		} else {
+			$flash = array();
+		}
+		return $flash;	
+	}
+	
+}

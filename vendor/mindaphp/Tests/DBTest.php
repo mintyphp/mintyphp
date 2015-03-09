b7ccdd73d84a4c56c8faf037be609e122c8c445f
Maurits van der Schee <maurits@vdschee.nl>
2015-03-09 23:46:03 +0100

    Fixed tests to reflect new error behavior

--- vendor/mindaphp/Tests/DBTest.php
+++ vendor/mindaphp/Tests/DBTest.php
@@ -15,13 +15,13 @@ class DBTest extends \PHPUnit_Framework_TestCase
     public function testDropPostsBefore()
     {
         $result = DB::query('DROP TABLE IF EXISTS `posts`;');
-        $this->assertNotFalse($result, 'drop posts failed: '.DB::$error);
+        $this->assertNotFalse($result, 'drop posts failed');
     }
 
     public function testDropUsersBefore()
     {
         $result = DB::query('DROP TABLE IF EXISTS `users`;');
-        $this->assertNotFalse($result, 'drop users failed: '.DB::$error);
+        $this->assertNotFalse($result, 'drop users failed');
     }    
 
     public function testCreateUsers()
@@ -35,7 +35,7 @@ class DBTest extends \PHPUnit_Framework_TestCase
             PRIMARY KEY (`id`),
             UNIQUE KEY `username` (`username`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;');
-        $this->assertNotFalse($result, 'create users failed: '.DB::$error);
+        $this->assertNotFalse($result, 'create users failed');
     }
 
     public function testCreatePosts()
@@ -54,26 +54,26 @@ class DBTest extends \PHPUnit_Framework_TestCase
             KEY `user_id` (`user_id`),
             CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;');
-        $this->assertNotFalse($result, 'create posts failed: '.DB::$error);
+        $this->assertNotFalse($result, 'create posts failed');
     }
 
     public function testInsertUsers()
     {
         $result = DB::insert("INSERT INTO `users` (`id`, `username`, `password`, `salt`, `created`) VALUES (NULL, 'test1', 'c32ac6310706acdadea74c901c3f08fe06c44c61', 'd7e8541887cb9b3461d7364e4e7c8b7d', '2014-05-28 22:58:22');");
-        $this->assertNotFalse($result, 'insert user failed 1: '.DB::$error);
+        $this->assertNotFalse($result, 'insert user failed 1');
         $this->assertEquals(1, $result);
         $result = DB::insert("INSERT INTO `users` (`id`, `username`, `password`, `salt`, `created`) VALUES (NULL, 'test2', 'c32ac6310706acdadea74c901c3f08fe06c44c61', 'd7e8541887cb9b3461d7364e4e7c8b7d', '2014-05-28 22:58:22');");
-        $this->assertNotFalse($result, 'insert user failed 2: '.DB::$error);
+        $this->assertNotFalse($result, 'insert user failed 2');
         $this->assertEquals(2, $result);
     }
     
     public function testInsertPosts()
     {
         $result = DB::insert("INSERT INTO `posts` (`id`, `slug`, `tags`, `title`, `content`, `created`, `published`, `user_id`) VALUES (NULL, '2014-08-test1', '', 'test', 'test', '0000-00-00 00:00:00', NULL, 1);");
-        $this->assertNotFalse($result, 'insert post failed 1: '.DB::$error);
+        $this->assertNotFalse($result, 'insert post failed 1');
         $this->assertEquals(1, $result);
         $result = DB::insert("INSERT INTO `posts` (`id`, `slug`, `tags`, `title`, `content`, `created`, `published`, `user_id`) VALUES (NULL, '2014-08-test2', '', 'test', 'test', '0000-00-00 00:00:00', NULL, 1);");
-        $this->assertNotFalse($result, 'insert post failed 2: '.DB::$error);
+        $this->assertNotFalse($result, 'insert post failed 2');
         $this->assertEquals(2, $result);
     }    
     
@@ -88,8 +88,8 @@ class DBTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(array('posts','users'), array_keys($result[0]));
         $this->assertEquals('id', array_shift(array_keys($result[0]['posts'])));
         $this->assertEquals('test1', $result[0]['users']['username']);
+        $this->setExpectedException('MindaPHP\DBError');
         $result = DB::select("some bogus query;");
-        $this->assertEquals(false, $result);
     }
     
     public function testSelectOne()
@@ -99,8 +99,8 @@ class DBTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('id', array_shift(array_keys(array_shift($result))));
         $result = DB::selectOne("SELECT * FROM `posts` WHERE slug like 'm%' limit 1;");
         $this->assertEquals(array(), $result);
+        $this->setExpectedException('MindaPHP\DBError');
         $result = DB::selectOne("some bogus query;");
-        $this->assertEquals(false, $result);
     }
     
     public function testSelectValues()
@@ -109,8 +109,8 @@ class DBTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(array('test1','test2'), $result);
         $result = DB::selectValues("SELECT username FROM `users` WHERE username like 'm%' limit 1;");
         $this->assertEquals(array(), $result);
+        $this->setExpectedException('MindaPHP\DBError');
         $result = DB::selectValues("some bogus query;");
-        $this->assertEquals(false, $result);
     }
     
     public function testSelectValue()
@@ -119,44 +119,43 @@ class DBTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('test1', $result);
         $result = DB::selectValue("SELECT username FROM `users` WHERE username like 'm%' limit 1;");
         $this->assertEquals(false, $result);
+        $this->setExpectedException('MindaPHP\DBError');
         $result = DB::selectValue("some bogus query;");
-        $this->assertEquals(false, $result);
     }
     
     public function testQuery()
     {
         $result = DB::query("SELECT * FROM `posts` limit 1;");
         $this->assertEquals(true, $result);
+        $this->setExpectedException('MindaPHP\DBError');
         $result = DB::query("some bogus query;");
-        $this->assertEquals(false, $result);
-        $this->assertTrue(is_string(DB::$error));
     }
         
         
     public function testDeletePosts()
     {
         $result = DB::delete('DELETE FROM `posts`;');
-        $this->assertNotFalse($result, 'delete posts failed: '.DB::$error);
+        $this->assertNotFalse($result, 'delete posts failed');
         $this->assertEquals(2, $result);
     }
     
     public function testDeleteUsers()
     {
         $result = DB::delete('DELETE FROM `users`;');
-        $this->assertNotFalse($result, 'delete users failed: '.DB::$error);
+        $this->assertNotFalse($result, 'delete users failed');
         $this->assertEquals(2, $result);
     }
         
     public function testDropPosts()
     {
         $result = DB::query('DROP TABLE `posts`;');
-        $this->assertNotFalse($result, 'drop posts failed: '.DB::$error);
+        $this->assertNotFalse($result, 'drop posts failed');
     }
     
     public function testDropUsers()
     {
         $result = DB::query('DROP TABLE `users`;');
-        $this->assertNotFalse($result, 'drop users failed: '.DB::$error);
+        $this->assertNotFalse($result, 'drop users failed');
     }
     
 }

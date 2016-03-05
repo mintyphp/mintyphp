dd4f22bd1259f2e4c6ceb832d4fcd1ce5ce95aec
Maurits van der Schee <maurits@vdschee.nl>
2016-03-05 20:47:57 +0100

    Move token functionality

--- vendor/mindaphp/Token.php
+++ vendor/mindaphp/Token.php
@@ -18,33 +18,33 @@ class Token {
 			foreach ($_SERVER as $key => $value) if (substr($key, 0, 5) == 'HTTP_') {
 				$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))))] = $value;
 			}
-		} 
+		}
 		return $headers;
 	}
-	
-	protected static function getToken($headers) {
+
+	protected static function getTokenFromHeaders($headers) {
 		$parts = explode(' ',trim($headers['Authorization']),2);
 		if (count($parts)!=2) return false;
 		if ($parts[0]!='Bearer') return false;
 		return explode('.',$parts[1],3);
 	}
-	
+
 	protected static function verifyToken($token,$algorithm,$secret) {
 		$algorithms(
 			'HS256'=>'sha256',
 			'HS384'=>'sha384',
 			'HS512'=>'sha512',
-		);		
+		);
 		if (!isset($algorithms[$algorithm])) return false;
 		$hmac = $algorithms[$algorithm];
 		if (count($token)<3) return false;
 		$header = json_decode(base64_decode($token[0]));
-		$signature = bin2hex(base64_decode($token[2]));	
+		$signature = bin2hex(base64_decode($token[2]));
 		if ($header['typ']!='JWT') return false;
 		if ($header['algo']!=$algorithm) return false;
 		return $signature==hash_hmac($hmac,"$token[0].$token[1]",$secret);
 	}
-	
+
 	protected static function verifyClaims($claims,$time,$leeway,$ttl) {
 		if (isset($claims['nbf']) && $time+$leeway<$claims['nbf']) return false;
 		if (isset($claims['iat']) && $time+$leeway<$claims['iat']) return false;
@@ -52,20 +52,22 @@ class Token {
 		if (isset($claims['exp']) && $time-$leeway>$claims['exp']) return false;
 		return true;
 	}
-	
-	protected static function getVerifiedClaims($headers,$time,$leeway,$ttl,$algorithm,$secret) {
-		$token = static::getToken($headers);
-		if (!$token) return false;
+
+	protected static function getVerifiedClaims($token,$time,$leeway,$ttl,$algorithm,$secret) {
 		if (!static::verifyToken($token,$algorithm,$secret)) return false;
 		$claims = json_decode(base64_decode($token[1]));
 		if (!$claims) return false;
 		if (!static::verifyClaims($claims,$time,$leeway,$ttl)) return false;
 		return $claims;
 	}
-	
+
 	public static function getClaims() {
 		if (static::$cache===null) {
+			// get from httponly cookie?
 			$headers = static::getHeaders();
+			if (!$headers) return false;
+			$token = static::getTokenFromHeaders($headers);
+			if (!$token) return false;
 			$time = time();
 			$leeway = static::$leeway;
 			$ttl = static::$ttl;

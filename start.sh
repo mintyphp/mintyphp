f1ad5ae9492e3d3d5f501d98f469655affcff4a7
Mark van Driel <mvdriel@nlware.com>
2018-01-14 13:17:06 +0100

    Fixed paths in start and test

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$(readlink -f "$0")")"
 php tools/requirements.php
 if [[ $? != 0 ]] ; then
     exit
 fi
-php -t web/ -S localhost:8000 tools/server.php
+php -t ../../../web/ -S localhost:8000 tools/server.php
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd "$(dirname "$(readlink -f "$0")")"
 php phpunit.phar --bootstrap src/Loader.php src/Tests

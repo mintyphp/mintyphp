98aa6c35128b90dcdec91c124c96de60290ae8e8
Maurits van der Schee <maurits@vdschee.nl>
2018-10-25 18:08:54 +0200

    fix

--- tools/adminer.php
+++ tools/adminer.php
@@ -1,6 +1,6 @@
 <?php
 // Change directory to project root
-chdir(__DIR__.'/..');
+chdir(__DIR__ . '/..');
 // Use default autoload implementation
 require 'vendor/mindaphp/core/Loader.php';
 // Load the config parameters
@@ -12,28 +12,37 @@ $_GET["username"] = "";
 $_GET["db"] = MindaPHP\Config\DB::$database;
 
 // Adminer Extension
-function adminer_object() {
+function adminer_object()
+{
 
-	class AdminerSoftware extends Adminer {
+    class AdminerSoftware extends Adminer
+    {
 
-		public function credentials() {
-			return array(\MindaPHP\Config\DB::$host, \MindaPHP\Config\DB::$username, \MindaPHP\Config\DB::$password);
-		}
+        public function credentials()
+        {
+            return array(\MindaPHP\Config\DB::$host, \MindaPHP\Config\DB::$username, \MindaPHP\Config\DB::$password);
+        }
 
-		public function database() {
-			return \MindaPHP\Config\DB::$database;
-		}
+        public function database()
+        {
+            return \MindaPHP\Config\DB::$database;
+        }
 
-		public function navigation($missing) {
-			parent::navigation($missing);
-			echo '<p class="links"><a href="/conventionist.php">Conventionist</a></p>';			
-		}
-		
-	}
+        public function navigation($missing)
+        {
+            parent::navigation($missing);
+            echo '<p class="links"><a href="/conventionist.php">Conventionist</a></p>';
+        }
 
-	return new AdminerSoftware;
+        public function login($username, $password)
+        {
+            return true;
+        }
+
+    }
+
+    return new AdminerSoftware;
 
 }
-
 
 include 'tools/latest.php';

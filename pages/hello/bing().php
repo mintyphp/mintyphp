e20043e71f48ef76709299fdd1abff681f7ce925
Maurits van der Schee <maurits@vdschee.nl>
2018-10-26 03:11:31 +0200

    update

--- pages/hello/bing().php
+++ pages/hello/bing().php
@@ -1,23 +1,24 @@
-<?php 
-$query = isset($_POST['q'])?$_POST['q']:'';
+<?php
+$query = isset($_POST['q']) ? $_POST['q'] : '';
 
 $results = array();
 if ($query) {
-	if (!($results = Cache::get($query))) {
-		if (Curl::call('GET','http://www.bing.com/search',array('q'=>$query),$result)==200) {
-			
-			$dom = new DOMDocument();
-			@$dom->loadHTML($result);
-			
-			$xpath = new DOMXpath($dom);
-			$elements = $xpath->query('//ol["b_results"]/li[@class="b_algo"]//h2/a');
-	
-			foreach ($elements as $element) {
-				$text = $element->nodeValue;
-				$link = $element->getAttribute("href");
-				$results[] = compact('text','link'); 
-			}
-			Cache::set($query,$results,60);
-		}
-	}
+    if (!($results = Cache::get($query))) {
+        $result = Curl::call('GET', 'http://www.bing.com/search', array('q' => $query));
+        if ($result['status'] == 200) {
+
+            $dom = new DOMDocument();
+            @$dom->loadHTML($result['data']);
+
+            $xpath = new DOMXpath($dom);
+            $elements = $xpath->query('//ol["b_results"]/li[@class="b_algo"]//h2/a');
+
+            foreach ($elements as $element) {
+                $text = $element->nodeValue;
+                $link = $element->getAttribute("href");
+                $results[] = compact('text', 'link');
+            }
+            Cache::set($query, $results, 60);
+        }
+    }
 }

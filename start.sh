bafbb1206b47e151490a5ab7fc8badc7f8fc6131
Maurits van der Schee <maurits@vdschee.nl>
2025-12-15 23:31:49 +0100

    update

--- start.sh
+++ start.sh
@@ -3,9 +3,5 @@ if [ ! -f composer.phar ]; then
     wget https://getcomposer.org/composer.phar
 fi
 php composer.phar install
-php vendor/mintyphp/tools/requirements.php
-if [[ $? != 0 ]]; then
-    exit
-fi
 CWD=$(pwd)
 php -t web/ -S localhost:8000 $CWD/vendor/mintyphp/core/src/Tools/Server.php
